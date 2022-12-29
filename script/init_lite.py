ec3439c3c2b1570a4065cda81fc45ca79e6bff38
Chi Thanh Le <57496995+tle2001@users.noreply.github.com>
2022-12-29 22:38:22 +0700

    queries and do the thang to optimize

--- script/init_lite.py
+++ script/init_lite.py
@@ -7,17 +7,22 @@ import sqlite3
 
 # Main function
 def main():
-    # Create connection 
+    # Create connection and cursor
     cnn = sqlite3.connect('../sales.db')
+    # cursor = cnn.cursor()
 
     # Load the .csv files into SQLite database
+    # Later please try use try/except/finally to deal with errors
     names = ["accounts", "orders", "region", "sales_reps", "web_events"]
     for name in names:
-        n = pd.read_csv(f"../data/{name}.csv")
-        n.to_sql(f"{name}", cnn)
-        print(f"Completed loading {name}.csv to sales.db")
+        try:    
+            n = pd.read_csv(f"../data/{name}.csv")
+            n.to_sql(f"{name}", cnn)
+            print(f"Completed loading {name}.csv to sales.db")
+        except sqlite3.Error as error:
+            print(f"Just got error in adding {name} to the database. Error: {error}")
+    # Oops I've just realized that SQLite does not support adding constraint by alter table so ye, next time
 
-    # Relationship creation
 
 # Usual things: main program
 if __name__ == "__main__":
